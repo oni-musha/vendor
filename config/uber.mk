1fffe978637b9c48df51034ee8d0d3399591e21f
Chet Kener <Cl3Kener@gmail.com>
2015-03-13 09:47:59 -0400

    uber: More Cleanups
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- config/uber.mk
+++ config/uber.mk
@@ -10,16 +10,8 @@ ifeq (Linux,$(UNAME))
   HOST_OS := linux
 endif
 
-# Only use these compilers on linux host.
 ifeq (linux,$(HOST_OS))
-
-# Add extra libs for the compilers to use
-# Filter by TARGET_ARCH since we're pointing to ARCH specific compilers.
-# To use this on new devices define TARGET_ARCH in device makefile.
 ifeq (arm,$(TARGET_ARCH))
-export LD_LIBRARY_PATH := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-4.8/arch-arm/usr/lib
-export LIBRARY_PATH := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-4.8/arch-arm/usr/lib
-
 # Path to toolchain
 UBER_AND_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-4.8
 UBER_AND := $(shell $(UBER_AND_PATH)/bin/arm-linux-androideabi-gcc --version)
@@ -50,9 +42,6 @@ endif
 endif
 
 ifeq (arm64,$(TARGET_ARCH))
-export LD_LIBRARY_PATH := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-4.9/arch-arm64/usr/lib
-export LIBRARY_PATH := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-4.9/arch-arm64/usr/lib
-
 # Path to toolchain
 UBER_AND_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-4.9
 UBER_AND := $(shell $(UBER_AND_PATH)/bin/aarch64-linux-android-gcc --version)
