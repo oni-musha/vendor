284a5e6f1e3134ade40efbc75539b5907b762ab9
despairfactor <matthewalex@outlook.com>
2015-04-14 15:23:54 -0400

    Update shamu fingerprint

--- products/optipop_shamu.mk
+++ products/optipop_shamu.mk
@@ -30,7 +30,7 @@ PRODUCT_NAME := optipop_shamu
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 6
 PRODUCT_MANUFACTURER := motorola
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=shamu BUILD_FINGERPRINT="google/shamu/shamu:5.0.1/LRX22C/1602158:user/release-keys" PRIVATE_BUILD_DESC="shamu-user 5.0.1 LRX22C 1602158 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=shamu BUILD_FINGERPRINT="google/shamu/shamu:5.0.1/LMY47O/1602158:user/release-keys" PRIVATE_BUILD_DESC="shamu-user 5.0.1 LMY47O 1602158 release-keys"
 
 # Boot Animation
 PRODUCT_COPY_FILES += \
