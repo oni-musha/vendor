5e0c021c542f7668b18c12fca9187585ebeb63d7
Chet Kener <Cl3Kener@gmail.com>
2015-06-13 08:21:19 -0400

    Update Backuptool
    * Make sure to check prereq before using.  Credits to CM.
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- prebuilt/bin/backuptool.sh
+++ prebuilt/bin/backuptool.sh
@@ -5,6 +5,10 @@
 
 export C=/tmp/backupdir
 export S=/system
+export V=5.1
+
+# Scripts in /system/addon.d expect to find backuptool.functions in /tmp
+cp -f /tmp/install/bin/backuptool.functions /tmp
 
 # Preserve /system/addon.d in /tmp/addon.d
 preserve_addon_d() {
@@ -19,6 +23,14 @@ restore_addon_d() {
   rm -rf /tmp/addon.d/
 }
 
+# Proceed only if /system is the expected major and minor version
+check_prereq() {
+if ( ! grep -q "^ro.build.version.release=$V.*" /system/build.prop ); then
+  echo "Not backing up files from incompatible version: $V"
+  exit 127
+fi
+}
+
 # Execute /system/addon.d/*.sh scripts with $1 parameter
 run_stage() {
 for script in $(find /tmp/addon.d/ -name '*.sh' |sort -n); do
@@ -29,12 +41,14 @@ done
 case "$1" in
   backup)
     mkdir -p $C
+    check_prereq
     preserve_addon_d
     run_stage pre-backup
     run_stage backup
     run_stage post-backup
   ;;
   restore)
+    check_prereq
     run_stage pre-restore
     run_stage restore
     run_stage post-restore
