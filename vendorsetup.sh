62bc76205cbd503c22d18588ed9294e931f6f34e
Chet Kener <Cl3Kener@gmail.com>
2015-06-23 16:25:05 -0400

    Remove Some devices
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,3 @@
 add_lunch_combo optipop_hammerhead-user
 add_lunch_combo darkpop_hammerhead-user
 add_lunch_combo optipop_shamu-userdebug
-add_lunch_combo optipop_manta-userdebug
-add_lunch_combo optipop_flo-userdebug
-add_lunch_combo optipop_grouper-userdebug
-add_lunch_combo optipop_mako-userdebug
