f9f71a02ba807ac9c5c7656d543b834fb4d8bc80
DespairFactor <matthewdalex92@gmail.com>
2015-09-05 14:52:27 -0400

    products: Use correct config.mk and fingerprints for shamu

--- products/darkpop_shamu.mk
+++ products/darkpop_shamu.mk
@@ -2,14 +2,14 @@
 $(call inherit-product, device/moto/shamu/aosp_shamu.mk)
 
 # Inherit common product files.
-$(call inherit-product, vendor/darkpop/products/common.mk)
+$(call inherit-product, vendor/darkpop/config/common.mk)
 
 # Override AOSP build properties
 PRODUCT_NAME := darkpop_shamu
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 6
-PRODUCT_MANUFACTURER := motorola
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME="hammerhead" BUILD_FINGERPRINT="google/hammerhead/hammerhead:5.1.1/LYZ28J/2128181:user/release-keys" PRIVATE_BUILD_DESC="hammerhead-user 5.1.1 LYZ28J 2128181 release-keys"
+PRODUCT_MANUFACTURER := motorola 
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME="shamu" BUILD_FINGERPRINT="google/shamu/shamu:5.1.1/LYZ28J/2128181:user/release-keys" PRIVATE_BUILD_DESC="shamu-user 5.1.1 LYZ28J 2128181 release-keys"
 
 # Boot animation
 PRODUCT_COPY_FILES += \
